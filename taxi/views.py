66ae44d1169edead4943ea3183101fa34344696d
unknown <kgoncharenko83@gmail.com>
2026-04-27 22:24:21 +0200

    Fix some problems

--- taxi/views.py
+++ taxi/views.py
@@ -44,7 +44,7 @@ class CarUpdateView(LoginRequiredMixin, generic.UpdateView):
 
 class CarDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Car
-    success_url = reverse_lazy("taxi:manufacturer-list")
+    success_url = reverse_lazy("taxi:car-list")
     template_name = "taxi/car-confirm-delete.html"
 
 
